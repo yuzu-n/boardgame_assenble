2747b09419afac4953947d015186c5d430a1f5a0
Yoshiaki Hara <dr.yossien@gmail.com>
2019-12-02 22:42:57 +0900

    fixup at CleanWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MODE = "production";
 
 const enabledSourceMap = MODE === "development";
@@ -32,7 +32,7 @@ module.exports = {
         }
     ]},
     plugins: [
-        new CleanWebpackPlugin(['dist']),
+        new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
             filename: './css/[name]-[contentHash].css',
         }),
