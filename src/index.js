1d8e5b8c4cf48c3cf60b4ae7e69f8bbdd558e424
Yoshiaki Hara <dr.yossien@gmail.com>
2020-02-15 16:18:08 +0900

    delete unused files.

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import "./style.scss";
-import title_image from './images/title_image.png';
-import games_image from './images/games.jpg';
-
-let img = document.getElementById('top-image')
-img.src = title_image
-
-let games = document.getElementById('games-image')
-games.src = games_image
